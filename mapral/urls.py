2eaffe2886e215f25e4532218479530dbf63f60e
MapralGlobal <110474181+MapralGlobal@users.noreply.github.com>
2023-11-02 16:04:56 +0530

    Update urls.py

--- mapral/urls.py
+++ mapral/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
-    # path('mapadmin',include('core.urls.mapral.urls')),
+    path('mapadmin',include('core.urls.mapral.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
